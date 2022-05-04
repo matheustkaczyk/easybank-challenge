015547522fd9ca25745c72bf48fca69527a8022b
matheustkaczyk <matheus111904@hotmail.com>
2022-05-04 10:32:13 -0300

    Estruturando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import logo from '../images/logo.svg';
+import menu from '../images/icon-hamburguer.svg';
+
 export const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <img src={logo} alt="easybank-logo" />
+      <img src={menu} alt="menu" />
     </header>
   )
 }
