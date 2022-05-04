f784658c67279ce242763f28a28f6b2518b1c254
matheustkaczyk <matheus111904@hotmail.com>
2022-05-04 11:18:35 -0300

    Configurado estrutura e css dos elementos hero

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -7,7 +7,7 @@ export const Presentation = () => {
       <div className="hero-images">
         <img src={phones} alt="phone-mockups" />
       </div>
-      <div>
+      <div className="hero-text">
         <h1>Next generation digital banking</h1>
         <p>Take your finantial life online. Your EasyBank account will be a one-stop shop for spending, saving, budgeting, investing and much more.</p>
         <Button text='Request invite' type="button" />
