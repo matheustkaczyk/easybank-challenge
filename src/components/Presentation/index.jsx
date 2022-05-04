9a547c4b82d045b3f924d40cd53e3636741c27a3
matheustkaczyk <matheus111904@hotmail.com>
2022-05-04 10:49:13 -0300

    Componentizando button, renderizando na tela

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -1,14 +1,16 @@
 import phones from '../../images/image-mockups.png';
+import { Button } from '../Button';
 
 export const Presentation = () => {
   return (
-    <div>
+    <div className="presentation">
       <div>
         <img src={phones} alt="phone-mockups" />
       </div>
       <div>
         <h1>Next generation digital banking</h1>
         <p>Take your finantial life online. Your EasyBank account will be a one-stop shop for spending, saving, budgeting, investing and much more.</p>
+        <Button text='Request invite' type="button" />
       </div>
     </div>
   )
