5db6cffa46090bf60e23156888c885b40da4703e
matheustkaczyk <matheus111904@hotmail.com>
2022-05-23 10:01:03 -0300

    fix: mudando path novamente para ver se corrige

--- src/data.jsx
+++ src/data.jsx
@@ -1,24 +1,29 @@
+import currency from './images/image-currency.jpg';
+import restaurant from './images/image-restaurant.jpg';
+import plane from './images/image-plane.jpg';
+import confetti from './images/image-confetti.jpg';
+
 export const data =  [
   {
-  "image": "../../images/image-currency.jpg",
+  "image": currency,
   "author": "Claire Robinson",
   "title": "Receive money in any currency with no fees",
   "description": "The world is getting smaller and we're becoming more mobile. So why should you be forced to only use a single...",
 },
 {
-  "image": "../../images/image-restaurant.jpg",
+  "image": restaurant,
   "author": "Wilson Hutton",
   "title": "Treat yourself without worrying about money",
   "description": "Our simple budgeting featire allows you to separete out your spending ant set realistic limits each month. That means you..."
 },
 {
-  "image": "../../images/image-plane.jpg",
+  "image": plane,
   "author": "Wilson Hutton",
   "title": "Take your Easybank card wherever you go",
   "description": "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you..."
 },
 {
-  "image": "../../images/image-confetti.jpg",
+  "image": confetti,
   "author": "Claire Robinson",
   "title": "Our invite-only Beta account are now live!",
   "description": "After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site..."
